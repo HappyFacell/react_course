066ccabb609dc7178bdb0d299e9428793738399f
HappyFacell <is722820@iteso.mx>
2023-09-26 11:57:40 -0600

    Part3c

--- Part3/BackEnd/models/note.js
+++ Part3/BackEnd/models/note.js
@@ -1,31 +1,39 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-mongoose.set('strictQuery', false)
+mongoose.set("strictQuery", false);
 
-const url = process.env.MONGO_URI
+const url = process.env.MONGO_URI;
 
-console.log('connecting to', url)
+console.log("connecting to", url);
 
-mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("connected to MongoDB");
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+    console.log("error connecting to MongoDB:", error.message);
+  });
 
 const noteSchema = new mongoose.Schema({
-  content: String,
+  content: { 
+    type: String,
+    minlength: 5, 
+    required: true 
+  },
+  date: { 
+    type: Date, 
+    required: true 
+  },
   important: Boolean,
-  date: Date,
-})
+});
 
-noteSchema.set('toJSON', {
+noteSchema.set("toJSON", {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
-module.exports = mongoose.model('Note', noteSchema)
+module.exports = mongoose.model("Note", noteSchema);
