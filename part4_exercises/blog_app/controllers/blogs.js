e031b19c051c804de3c8b959ddae64d260c0b83a
Armando Ferreira <is722820@iteso.mx>
2023-12-01 10:51:08 -0600

    Fix blog app back

--- part4_exercises/blog_app/controllers/blogs.js
+++ part4_exercises/blog_app/controllers/blogs.js
@@ -1,74 +1,80 @@
-const jwt = require('jsonwebtoken')
-const blogsRouter = require('express').Router()
-const Blog = require('../models/blogs')
-const User = require('../models/users')
+const blogsRouter = require("express").Router();
+const Blog = require("../models/blogs");
 
-blogsRouter.get('/', async (request, response) => {
-  const blog = await Blog.find({}).populate('user', { username: 1, name: 1 })
-  response.json(blog)
-})
+const { userExtractor } = require("../utils/middlewares");
 
-blogsRouter.post('/', async (request, response) => {
-  const body = request.body
+blogsRouter.get("/", async (request, response) => {
+  const blog = await Blog.find({}).populate("user", { username: 1, name: 1 });
+  response.json(blog);
+});
 
-  const user = request.user
+blogsRouter.post("/", userExtractor,async (request, response) => {
+  const body = request.body;
 
-  console.log(user)
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes || 0,
-    user: user._id
-  })
+    likes: body.likes? body.likes : 0
+  });
+  
+  const user = request.user;
 
-  const savedBlog = await blog.save()
-  user.blogs = user.blogs.concat(savedBlog._id)
+  if(!user){
+    return response.status(401).json({error: 'operation not permitted'});
+  }
+
+  blog.user = user._id
+
+  const createdBlog = await blog.save()
+
+  user.blogs = user.blogs.concat(createdBlog._id)
 
   await user.save()
 
-  response.status(201).json(savedBlog)
-})
+  response.status(201).json(createdBlog);
+});
 
-blogsRouter.get('/:id', async (request, response) => {
-  const blog = await Blog.findById(request.params.id)
+blogsRouter.get("/:id", async (request, response) => {
+  const blog = await Blog.findById(request.params.id);
 
-  if (blog){
-    response.json(blog)
+  if (blog) {
+    response.json(blog);
+  } else {
+    response.status(404).end();
   }
-  else {
-    response.status(404).end()
-  }
-})
+});
 
-blogsRouter.delete('/:id', async (request, response) => {
+blogsRouter.delete("/:id", userExtractor,async (request, response) => {
+  const blog = await Blog.findById(request.params.id);
+  const user = request.user;
 
-  const user = request.user
-  const blog = await Blog.findById(request.params.id)
-
-  if(user._id.equals(blog.user)){
-    await Blog.findByIdAndDelete(request.params.id)
-    response.status(204).end()
-  }
-  else {
-    response.status(404).json({ error: 'error with deleted blog' })
+  if (user || blog.user.toString() !== user.id.toString()) {
+    return response.status(401).json({ error: "operation not permitted" });
   }
 
-})
+  user.blogs = user.blogs.filter((b) => b.toString() !== blog.id.toString());
 
-blogsRouter.put('/:id', async (request, response) => {
-  const body = request.body
+  await user.save();
+  await blog.remove();
+
+  response.status(204).end();
+});
+
+blogsRouter.put("/:id", async (request, response) => {
+  const body = request.body;
 
   const blog = {
     title: body.title,
     author: body.author,
     likes: body.likes,
-    url: body.url
-  }
+    url: body.url,
+  };
 
-  const updateBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
-  response.json(updateBlog)
+  const updateBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true,
+  });
+  response.json(updateBlog);
+});
 
-})
-
-module.exports = blogsRouter
+module.exports = blogsRouter;
